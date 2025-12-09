573bdee83d033692fb8a496b651c6a7110113595
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2025-12-09 09:19:27 +0000

    'resendapp'

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,26 +1,22 @@
-import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config(); // IMPORTANT
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
   try {
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-});
-    await transporter.sendMail({
-      from: `"J Movies App" <${process.env.MAIL_USER}>`,
+    const data = await resend.emails.send({
+      from: process.env.SENDER_EMAIL,
       to,
       subject,
       html,
     });
 
-    console.log("Email sent successfully");
+    console.log("MAIL SENT:", data);
   } catch (error) {
-    console.error("Email sending error:", error);
+    console.error("Resend email error:", error);
   }
 };
 
