f72ced0815c9e731f1b7a6d799c008bfa4040377
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2025-12-09 13:38:39 +0000

    fix: include env file

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,25 +3,25 @@ import nodemailer from "nodemailer";
 const sendEmail = async (to, subject, html) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: process.env.SMTP_HOST,
+      port: Number(process.env.SMTP_PORT),
+      secure: false, // Mailtrap works only with secure=false
       auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
       },
     });
 
     await transporter.sendMail({
-      from: `"J Movies App" <${process.env.MAIL_USER}>`,
+      from: `"J Movies App" <${process.env.SMTP_USER}>`,
       to,
       subject,
       html,
     });
 
-    console.log("Email sent using Gmail SMTP");
+    console.log("✅ Email sent using Mailtrap SMTP");
   } catch (error) {
-    console.error("Email sending error:", error);
+    console.error("❌ Email sending error:", error);
   }
 };
 
