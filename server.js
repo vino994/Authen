84a55af90ea80a9c5e826d8db5469fb5f2f64c61
vinothkumar sanjeevi <vinodjayasudha@gmail.com>
2025-12-09 07:31:10 +0000

    Added test-mail route

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
-
+import sendEmail from "./utils/sendEmail.js";
 dotenv.config();
 const app = express();
 connectDB();
@@ -29,6 +29,14 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => res.send("Password Reset API Working"));
+app.get("/test-mail", async (req, res) => {
+  try {
+    await sendEmail("vinodjayasudha@gmail.com", "Render SMTP Test", "<h1>Working!</h1>");
+    res.send("Mail sent from Render!");
+  } catch (err) {
+    res.send("Error: " + err.message);
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server running on", PORT));
